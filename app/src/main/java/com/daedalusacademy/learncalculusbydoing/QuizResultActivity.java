ee1af40d9af5f1560bafe426fc1b11fc08cb5a9a
westgoten <rodrigodeoliveira397@gmail.com>
2018-12-12 14:38:41 -0300

    Changed app theme's colors; Changed buttons' style; Changed QuizResultActivity message's style.

--- app/src/main/java/com/daedalusacademy/learncalculusbydoing/QuizResultActivity.java
+++ app/src/main/java/com/daedalusacademy/learncalculusbydoing/QuizResultActivity.java
@@ -2,6 +2,9 @@ package com.daedalusacademy.learncalculusbydoing;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,6 +14,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class QuizResultActivity extends AppCompatActivity {
     private static final String EXTRA_SCORE = "com.daedalusacademy.learncalculusbydoing.EXTRA_SCORE";
 
+    @SuppressWarnings("deprecation")
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,7 +24,10 @@ public class QuizResultActivity extends AppCompatActivity {
         int score = resultIntent.getIntExtra(EXTRA_SCORE, 0);
 
         TextView scoreView = findViewById(R.id.result_score_text_view);
-        scoreView.setText(String.valueOf(score));
+        SpannableString string = new SpannableString(String.format(getString(R.string.score), score, 10));
+        string.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.primaryColor)), 0, 1,
+                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        scoreView.setText(string);
 
         Button button = findViewById(R.id.result_to_menu_button);
         button.setOnClickListener(new View.OnClickListener() {
